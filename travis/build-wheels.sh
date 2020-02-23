df4c569419e0392e0212df32b7a19604ce53c44d
Clemens <chofreither@gmail.com>
2020-02-23 01:32:02 +0100

    install nose

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -10,7 +10,7 @@ git clone https://github.com/c-f-h/ilupp.git
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     #"${PYBIN}/pip" install -r /io/dev-requirements.txt
-    "${PYBIN}/pip" install pybind11
+    "${PYBIN}/pip" install nose pybind11
     "${PYBIN}/pip" wheel /io/ilupp/ -w wheelhouse/
 done
 
