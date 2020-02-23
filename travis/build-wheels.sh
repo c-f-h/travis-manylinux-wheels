bd7d8dccf9c67fb52dfb925ceec3d52b6d2537f4
Clemens <chofreither@gmail.com>
2020-02-23 01:48:08 +0100

    change test running

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -22,6 +22,5 @@ done
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install ilupp --no-index -f /io/wheelhouse
-    #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
-    "${PYBIN}/nosetests" ilupp/test/
+    "${PYBIN}/nosetests" /io/ilupp/test/
 done
